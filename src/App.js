e5c57dd4122212f81fb74ccb8cd28e744ec733a0
PabloValles <valles.pablo@hotmail.com>
2021-07-10 10:38:00 -0300

    Dando formato a la pagina con flex y creando boton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from './logo.svg';
+import styled from '@emotion/styled'
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 1px solid #000;
+`
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`
 
 function App() {
+
+  const consultarApi = () => {
+    console.log("Consultando")
+  }
+
   return (
-    <h1>Breaking bad</h1>
+      <Contenedor>
+        <Boton
+          onClick={consultarApi}
+        >Obtener frase</Boton>
+
+      </Contenedor>
   );
 }
 
